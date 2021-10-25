d00e9d0d5c7ebf4aea9cc459d4f25e2af79b95f6
Vincent Kuhlmann <vincent.kuhlmann@hotmail.com>
2021-10-25 23:01:04 +0200

    Add some content for Week 3

---
+++ presentatie/composition/2021-09-Cursus/Week3/CreatePresentatieFormat.bat
@@ -0,0 +1,5 @@
+pdftex -ini -jobname="presentatie" "&pdflatex" mylatexformat.ltx presentatie.tex
+move presentatie.fmt auxdir/presentatie.fmt
+move presentatie.log auxdir/presentatieFmt.log
+del presentatie.pdf
+PAUSE
