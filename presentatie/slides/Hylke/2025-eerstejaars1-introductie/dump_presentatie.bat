be5f7dfed55926c02ec4059a8068282e1b7e8562
hylkeHoogeveen117 <hylkehoogeveen117@gmail.com>
2025-10-29 18:32:20 +0100

    mijn slides voor de sjaars cursus

--- presentatie/slides/Hylke/2025-eerstejaars1-introductie/dump_presentatie.bat
+++
@@ -1,5 +0,0 @@
-pdftex -ini -jobname="presentatie" "&pdflatex" mylatexformat.ltx presentatie.tex
-rem move presentatie.fmt auxdir/presentatie.fmt
-rem move presentatie.log auxdir/presentatieFmt.log
-del presentatie.pdf
-
